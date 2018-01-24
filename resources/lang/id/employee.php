d29b815faa6f9bcc5a03320b06e63d8042b5249a
ROERY <ROERY>
2018-01-25 00:06:33 +0700

    Bimbingan

---
+++ resources/lang/id/employee.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+	'list_employees' => 'Daftar Pengakses',
+	'new_employee' => 'Pengakses Baru',
+	'person_id' => 'ID Pengakses',
+	'name' => 'Nama',
+	'email' => 'Email',
+	'password' => 'Password',
+	'confirm_password' => 'Ulangi Password',
+	'submit' => 'Submit',
+	'edit' => 'Ganti',
+	'delete' => 'Hapus',
+	'update_employee' => 'Ganti Pengakses'
+
+];
---
+++ resources/lang/id/menu.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+	'dashboard' 			=> 'Dashboard',
+	'customers' 			=> 'Pelanggan',
+	'employees'				=> 'Pengakses',
+	'items' 				=> 'Barang',
+	'item_kits' 			=> 'Barang Paket',
+	'suppliers' 			=> 'Supplier',
+	'receivings' 			=> 'Penerimaan',
+	'sales' 				=> 'Penjualan',
+	'reports' 				=> 'Laporan',
+	'receivings_report' 	=> 'Laporan Pembelian',
+	'sales_report' 			=> 'Laporan Penjualan',
+	'logout'				=> 'Keluar',
+	'application_settings' 	=> 'Pengaturan'
+
+];
